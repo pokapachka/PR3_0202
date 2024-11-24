1f7ea4adb485444b6ca26507181cb62906df36c9
pokapachka <pachkaosokin@yandex.ru>
2024-11-24 19:56:04 +0500

    Метод прослушивания ответов от сервера. Осокин

--- SnakeWPF/MainWindow.xaml.cs
+++ SnakeWPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,6 +12,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -38,6 +41,59 @@ namespace SnakeWPF
         {
             tRec = new Thread(new ThreadStart(Receiver));
             tRec.Start();
+        }
+        public void OpenPage(Page PageOpen)
+        {
+            DoubleAnimation startAnimation = new DoubleAnimation();
+            startAnimation.From = 1;
+            startAnimation.To = 0;
+            startAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            startAnimation.Completed += delegate
+            {
+                frame.Navigate(PageOpen);
+                DoubleAnimation endAnimation = new DoubleAnimation();
+                endAnimation.From = 0;
+                endAnimation.To = 1;
+                endAnimation.Duration = TimeSpan.FromSeconds(0.6);
+                frame.BeginAnimation(OpacityProperty, endAnimation);
+            };
+            frame.BeginAnimation(OpacityProperty, startAnimation);
+        }
+        public void Receiver()
+        {
+            receivingUdpClient = new UdpClient(int.Parse(ViewModelUserSettings.Port));
+            IPEndPoint RemoteIpEndPoint = null;
+            try
+            {
+                while (true)
+                {
+                    byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+                    string returnData = Encoding.UTF8.GetString(receiveBytes);
+                    if (ViewModelGames == null)
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            OpenPage(Game);
+                        }); 
+                    }
+                    ViewModelGames = JsonConvert.DeserializeObject<ViewModelGames>(returnData.ToString());
+                    if (ViewModelGames.SnakesPlayers.GameOver)
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            OpenPage(new Pages.EndGame());
+                        });
+                    }
+                    else
+                    {
+                        Game.CreateUI();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Возникло исключение: " + ex.ToString() + "\n" + ex.Message);
+            }
         }
     }
 }
