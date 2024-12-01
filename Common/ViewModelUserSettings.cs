f79ca76fe52113174a7288b04b5e63cc6cc8798e
aetheross <is-21-2-10@permaviat.ru>
2024-12-01 15:22:37 +0500

    A few initial changes. Created projects Common and Snake (server).

---
+++ Common/ViewModelUserSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common
+{
+    public class ViewModelUserSettings
+    {
+        public String IPAddress { get; set; }
+        public String Port { get; set; }
+        public String Name { get; set; }
+        public Int32 IdSnake = -1;
+    }
+}
